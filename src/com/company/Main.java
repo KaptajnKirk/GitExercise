0ac6c8d5c409c8bfb18f3111150d687011f8b5ec
KaptajnKirk <theodor@duus-kirk.dk>
2022-02-25 14:31:41 +0100

    Revert "New Name"
    This reverts commit 994bbfce

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,12 +6,19 @@ public class Main {
 
     Scanner sc = new Scanner(System.in);
 
+    public void greet () {
 
+        System.out.println("Please input your name: ");
+        String name = sc.next();
+
+        System.out.println("Hey  " + name);
+
+    }
 
     public static void main(String[] args) {
 
         Main obj = new Main();
-
+        obj.greet();
 
         System.out.println("It works!");
 
