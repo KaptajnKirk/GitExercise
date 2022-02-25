7c4a5af0cad9142ad34ac4b6662c8a8455c940dc
KaptajnKirk <theodor@duus-kirk.dk>
2022-02-25 13:24:02 +0100

    Added greet

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,25 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
+
+    Scanner sc = new Scanner(System.in);
+
+    public void greet () {
+
+        System.out.println("Please input your name: ");
+        int name = sc.nextInt();
+
+    }
 
     public static void main(String[] args) {
 
+        Main obj = new Main();
+        obj.greet();
+
         System.out.println("It works!");
+
     }
+
 }
