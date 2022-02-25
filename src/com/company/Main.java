6a6f2cdc8d8c3db0e774d0ab94d9fac1b3355cbd
KaptajnKirk <theodor@duus-kirk.dk>
2022-02-25 14:30:33 +0100

    New Name2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,19 +6,12 @@ public class Main {
 
     Scanner sc = new Scanner(System.in);
 
-    public void halliHallo () {
 
-        System.out.println("Please input your name: ");
-        String name = sc.next();
-
-        System.out.println("Hey  " + name);
-
-    }
 
     public static void main(String[] args) {
 
         Main obj = new Main();
-        obj.greet();
+
 
         System.out.println("It works!");
 
